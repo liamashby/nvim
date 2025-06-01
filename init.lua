874334774b526e69b800c110e563e4f80c25f220
Liam Ashby <liam@Liams-MacBook-Air.local>
2025-06-01 19:32:34 +1000

    first commit

---
+++ init.lua
@@ -0,0 +1,43 @@
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=2")
+vim.cmd("set shiftwidth=2")
+vim.g.mapleader = " "
+
+-- Bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out, "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
+end
+vim.opt.rtp:prepend(lazypath)
+
+local plugins = {
+	{ "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+	{
+	  'nvim-telescope/telescope.nvim', tag = '0.1.8',
+	   dependencies = { 'nvim-lua/plenary.nvim' }
+	}
+}
+local opt = {}
+
+-- Setup lazy.nvim
+require("lazy").setup({plugins, opts
+})
+
+-- Setup telecope
+local builtin = require("telescope.builtin")
+vim.keymap.set('n', '<C-p>', builtin.find_files, {})
+
+-- Setup colourscheme
+require("catppuccin").setup()
+vim.cmd.colorscheme "catppuccin"
