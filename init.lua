8a124e5ed241e008af4a51fba76f5c2ba710175a
Liam Ashby <liam@Liams-MacBook-Air.local>
2025-06-02 19:05:26 +1000

    moved plugins to lua folder

--- init.lua
+++ init.lua
@@ -21,32 +21,10 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-local plugins = {
-	{ "catppuccin/nvim", name = "catppuccin", priority = 1000 },
-	{
-	  'nvim-telescope/telescope.nvim', tag = '0.1.8',
-	   dependencies = { 'nvim-lua/plenary.nvim' }
-	},
-  {"nvim-treesitter/nvim-treesitter", branch = 'master', lazy = false, build = ":TSUpdate"},
-  {
-    "nvim-neo-tree/neo-tree.nvim",
-    branch = "v3.x",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-tree/nvim-web-devicons", 
-      "MunifTanjim/nui.nvim",
-    },
-    lazy = false, 
-    opts = {
-    },
-  }
-}
-
 local opt = {}
 
 -- Setup lazy.nvim
-require("lazy").setup({plugins, opts
-})
+require("lazy").setup("plugins")
 
 -- Setup telecope
 local builtin = require("telescope.builtin")
