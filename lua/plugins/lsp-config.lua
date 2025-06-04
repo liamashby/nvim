4b3bec84021639872a808833bf05edb83d228374
Liam Ashby <liam@Liams-MacBook-Air.local>
2025-06-04 17:04:04 +1000

    added more broken lsp config

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,7 +1,33 @@
 return {
+  {
     "mason-org/mason.nvim",
     opts = {},
     config = function()
       require("mason").setup() 
     end
+  },
+  {
+    "mason-org/mason-lspconfig.nvim",
+    opts = {
+        ensure_installed = { "lua_ls" },
+    },
+    dependencies = {
+        { "mason-org/mason.nvim", opts = {} },
+        "neovim/nvim-lspconfig",
+    },
+    config = function()
+      require("mason-lspconfig").setup()
+    end
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function ()
+      vim.lsp.enable('lua_ls')
+      vim.lsp.config('lua_ls', {
+        settings = {
+          ['lus_ls'] = {},
+          },
+        })
+    end
+  }
 }
