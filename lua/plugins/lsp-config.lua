9982f8c43a27b858bce2ce8610729699c1c7ef45
Liam Ashby <liam@Liams-MacBook-Air.local>
2025-06-10 21:37:55 +1000

    added ensured installed to lsp config

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -8,22 +8,29 @@ return {
   },
   {
     "mason-org/mason-lspconfig.nvim",
-    opt = {},
-    dependencies = {
-        { "mason-org/mason.nvim", opts = {} },
-        "neovim/nvim-lspconfig",
-    }
+    config = function()
+      require("mason-lspconfig").setup(
+      { ensure_installed = {"lua_ls", "pylsp"}})
+    end
   },
   {
     "neovim/nvim-lspconfig",
     config = function ()
       vim.lsp.enable('lua_ls')
+      vim.lsp.enable('pylsp')
       vim.lsp.config('lua_ls', {
         settings = {
           ['lus_ls'] = {},
           },
         })
+      vim.lsp.config('pylsp', {
+        settings = {
+          ['python-lsp-server'] = {},
+          },
+        })
       vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
+      vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
+      vim.keymap.set({'n', 'v'}, '<leader>ca', vim.lsp.buf.code_action, {})
     end
   }
 }
