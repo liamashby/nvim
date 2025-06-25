5a8ebc2eedbcaa754b7e7e2dfe49102bea400a72
Liam Ashby <liam@Liams-MacBook-Air.local>
2025-06-25 20:13:46 +1000

    added go to tree sitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -6,7 +6,7 @@ return {
   config = function()
     local config = require("nvim-treesitter.configs")
     config.setup({
-      ensure_installed = {"lua", "python"},
+      ensure_installed = {"lua", "python", "go"},
       highlight = { enable = true },
       indent = { enable = true },
     })
